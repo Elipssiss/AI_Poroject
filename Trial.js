05471d9e362ef09a0712760954a62ed1ce1f127a
Elipssiss <85240541+Elipssiss@users.noreply.github.com>
2021-06-02 15:30:01 +0100

    Add files via upload

---
+++ Trial.js
@@ -0,0 +1,53 @@
+const CanvasSize = 1000
+const GridSize = 20
+const CellSize = 50
+const FR = 5
+
+var agent = {x:0, y:0}
+
+function Goal(){
+    var x_goal = Math.round(Math.floor(Math.random()*GridSize*CellSize)/CellSize)*CellSize
+    var y_goal = Math.round(Math.floor(Math.random()*GridSize*CellSize)/CellSize)*CellSize
+    fill(color(50, 168, 82))
+    rect(x_goal,y_goal,CellSize,CellSize)
+
+}
+       
+function Start(){
+    agent.x = Math.round(Math.floor(Math.random()*GridSize*CellSize)/CellSize)*CellSize
+    agent.y = Math.round(Math.floor(Math.random()*GridSize*CellSize)/CellSize)*CellSize
+    fill(color(102, 204, 255))
+    rect(agent.x,agent.y,CellSize,CellSize)
+
+}
+
+function setup(){
+    let canvas = createCanvas(CanvasSize,CanvasSize)
+    canvas.parent("sketch-holder")
+    frameRate(FR)
+    printgrid(GridSize, GridSize, CellSize)
+    Goal()
+    Start()
+}
+
+function printgrid(height, length, CellSize){
+    fill(color(255, 255, 255))
+    for (var x=0; x<height; x+=1){
+        for (var y=0; y<length; y+=1){
+            var cell_x = x*CellSize
+            var cell_y = y*CellSize
+            rect(cell_y,cell_x,CellSize,CellSize)
+        }
+    }
+}   
+
+function draw(){
+    do{
+        agent.x += (Math.floor(Math.random()*3)-1)*50
+        agent.y += (Math.floor(Math.random()*3)-1)*50
+        fill(color(102, 204, 255))
+        rect(agent.x,agent.y,CellSize,CellSize)
+    }
+    while (agent.x != x_goal && agent.y != y_goal);
+
+}
